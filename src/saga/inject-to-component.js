83d6aad512038b68cee142e90c99cf193abd0fd2
Paul Sauer <paul@debitize.com>
2018-04-05 18:16:12 -0400

    Dont stop sagas on componentWillUnmount

--- src/saga/inject-to-component.js
+++ src/saga/inject-to-component.js
@@ -99,9 +99,9 @@ export default function injectSagasIntoClass (Klass, input, output) {
     if (DEBUG) {
       console.log('component will unmount')
     }
-    if (this._keaRunningSaga) {
-      cancelSaga(this._keaRunningSaga)
-    }
+    // if (this._keaRunningSaga) {
+    //   cancelSaga(this._keaRunningSaga)
+    // }
 
     originalComponentWillUnmount && originalComponentWillUnmount.bind(this)()
   }
